ad8393de45daaecaa6a8699d0db56d490b86e591
Osman Zakir <osmanzakir90@hotmail.com>
2020-02-02 02:01:21 +0500

    added comment to give credit to SO for regex

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 "use strict";
 
 const str = " .. \"test me\" .. \"Say \\\"Hello\\\"!\" .. \"\\\\ \\\" .. ";
-const regex = /"(?:\\"|.)*?"/g;
+const regex = /"(?:(?:\\"|[^"])*)"/g; // Taken from SO
 alert(str.match(regex));
